4fc0af12af399e27f7e75fe9bbd35ece9aa7f99f
Valerij Melguj <vj.melguj@gmail.com>
2024-01-30 20:21:07 +0300

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <input
+      placeholder="Найти сотрудника"
+      className="form-control search-input"
+      type="text"
+    />
+  );
+};
+export default SearchPanel;
