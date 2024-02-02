b130543d4255eef3134fa46dc23d9ccfbdcd9ac8
Valerij Melguj <vj.melguj@gmail.com>
2024-02-02 21:06:38 +0300

    new validation

--- src/components/app-form-validation-error/app-form-validation-error.js
+++ src/components/app-form-validation-error/app-form-validation-error.js
@@ -2,12 +2,13 @@ import { Component } from "react";
 import "./app-form-validation-error.css";
 
 class FormValidationError extends Component {
-  constructor(props) {
+   
+ /*  constructor(props) {
     super(props);
-  }
+  } */
 
   render() {
-    let nameError = this.props.name;
+    let nameError = this.props.errorMessage;
     return <span className="add-error">{nameError}</span>;
   }
 }
--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -10,6 +10,12 @@ class EmployersAddForm extends Component {
     this.state = {
       name: "",
       salary: "",
+      errors: {
+        nameError: false,
+        nameErrorText: "Не менее 3 символов!",
+        salaryError: false,
+        salaryErrorText: "Введите значение!",
+      },
     };
   }
 
@@ -22,7 +28,46 @@ class EmployersAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
 
-    if (this.state.name.length < 3 || !this.state.salary) {
+    const errors = this.state.errors;
+
+    if (this.state.name.length < 3) {
+      this.setState(() => ({
+        errors: {
+          nameError: true,
+          nameErrorText: errors.nameErrorText,
+        },
+      }));
+    } else if (!this.state.salary) {
+      this.setState(() => ({
+        errors: {
+          salaryError: true,
+          salaryErrorText: errors.salaryErrorText,
+        },
+      }));
+    } else {
+      const person = {
+        name: this.state.name,
+        salary: this.state.salary,
+        increase: false,
+        rise: false,
+        id: nanoid(),
+      };
+      this.props.onAdd(person);
+      this.setState(() => ({
+        name: "",
+        salary: "",
+        errors: {
+          nameError: false,
+          nameErrorText: "Не менее 3 символов!",
+          salaryError: false,
+          salaryErrorText: "Введите значение!",
+        },
+      }));
+    }
+
+    //=====================
+
+    /* if (this.state.name.length < 3 || !this.state.salary) {
       return false;
     } else {
       const person = {
@@ -37,35 +82,45 @@ class EmployersAddForm extends Component {
         name: "",
         salary: "",
       }));
-    }
+    } */
   };
 
   render() {
-    const { name, salary } = this.state;
+    const { name, salary, errors } = this.state;
     const { onSubmit } = this;
 
     return (
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
         <form onSubmit={onSubmit} className="add-form d-flex">
-          <input
-            value={name} // контролируемый элемент
-            type="text"
-            name="name"
-            className= 'form-control new-post-label'
-            placeholder="Как его зовут?"
-            onChange={this.onValueChange}
-          />
-          <input
-            value={salary} // контролируемый элемент
-            type="text"
-            className='form-control new-post-label'
-            placeholder="З/П в $?"
-            onChange={this.onValueChange}
-            name="salary"
-          />
+          <div className="flex-direction-column-container">
+            <input
+              value={name} // контролируемый элемент
+              type="text"
+              name="name"
+              className="form-control new-post-label"
+              placeholder="Как его зовут?"
+              onChange={this.onValueChange}
+            />
+            {errors.nameError && (
+              <FormValidationError errorMessage={errors.nameErrorText} />
+            )}
+          </div>
+          <div className="flex-direction-column-container">
+            <input
+              value={salary} // контролируемый элемент
+              type="text"
+              className="form-control new-post-label"
+              placeholder="З/П в $?"
+              onChange={this.onValueChange}
+              name="salary"
+            />
+            {errors.salaryError && (
+              <FormValidationError errorMessage={errors.salaryErrorText} />
+            )}
+          </div>
 
-          <button type="submit" className="btn btn-outline-light">
+          <button type="submit" className="btn btn-outline-light max-height">
             Добавить
           </button>
         </form>
